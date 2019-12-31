ca409448b551dac07a5a328995ae9f617f210e73
harsha <harshav17@gmail.com>
2019-12-31 15:21:29 -0800

    finishing user service and adding user client

---
+++ user-cli/go.mod
@@ -0,0 +1,10 @@
+module github.com/harshav17/shippy/user-cli
+
+go 1.13
+
+require (
+	github.com/harshav17/shippy v0.0.0-20191231203430-675f1f2a0e56
+	github.com/micro/cli v0.2.0
+	github.com/micro/go-micro v1.18.0
+	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553
+)
---
+++ user-service/repository.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	pb "github.com/harshav17/shippy/user-service/proto/user"
+	"github.com/jinzhu/gorm"
+)
+
+//Repository -
+type Repository interface {
+	GetAll() ([]*pb.User, error)
+	Get(id string) (*pb.User, error)
+	Create(user *pb.User) error
+	GetByEmailAndPassword(user *pb.User) (*pb.User, error)
+}
+
+//UserRepository -
+type UserRepository struct {
+	db *gorm.DB
+}
+
+//GetAll -
+func (repo *UserRepository) GetAll() ([]*pb.User, error) {
+	var users []*pb.User
+	if err := repo.db.Find(&users).Error; err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+//Get -
+func (repo *UserRepository) Get(id string) (*pb.User, error) {
+	var user *pb.User
+	user.Id = id
+	if err := repo.db.First(&user).Error; err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+//GetByEmailAndPassword -
+func (repo *UserRepository) GetByEmailAndPassword(user *pb.User) (*pb.User, error) {
+	if err := repo.db.First(&user).Error; err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+//Create -
+func (repo *UserRepository) Create(user *pb.User) error {
+	if err := repo.db.Create(user).Error; err != nil {
+		return err
+	}
+	return nil
+}
